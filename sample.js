64c86fb6f8bdf14a328fd1d05a3b08feac367031
kuccho524 <kucchachachanocha@gmail.com>
2026-04-19 21:27:05 +0900

    learn why typescript

---
+++ sample.js
@@ -0,0 +1,10 @@
+const user = {
+  userName: '田中',
+  userEmail: 'yanaka@example.com',
+  userAge: 30
+}
+
+console.log(user.userName);
+console.log(user.userEmail);
+console.log(user.userAge);
+console.log(user.name);
